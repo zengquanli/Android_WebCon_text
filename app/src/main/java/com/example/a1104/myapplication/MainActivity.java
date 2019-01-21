29137c2d7a967ab70c2ff98040fe09d3fc40c976
zengquan_li <1104430777@qq.com>
2019-01-21 14:20:45 +0800

    模仿code

--- app/src/main/java/com/example/a1104/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a1104/myapplication/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.a1104.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        button = (Button)findViewById(R.id.button);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view,String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl("http://www.baidu.com");
+
     }
 }
