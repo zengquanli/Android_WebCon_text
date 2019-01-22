31753202b2b13432802a29b96712ee98c361b366
zengquan_li <1104430777@qq.com>
2019-01-22 10:07:38 +0800

    卡在了getWebViewClient

--- app/src/main/java/com/example/a1104/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a1104/myapplication/MainActivity.java
@@ -7,12 +7,20 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webview;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webview = (WebView)findViewById(R.id.webview);
+        button = (Button)findViewById(R.id.button);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getWebViewClient(new WebViewClient(){
+
+        });
+
 
 
     }
