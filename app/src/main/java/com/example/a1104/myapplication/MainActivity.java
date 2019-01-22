38817789911caefccba6776179a1f2bda522034f
zengquan_li <1104430777@qq.com>
2019-01-22 09:55:29 +0800

    delete webview in java

--- app/src/main/java/com/example/a1104/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a1104/myapplication/MainActivity.java
@@ -7,25 +7,13 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webView);
-        button = (Button) findViewById(R.id.button);
 
-
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view,String url){
-                view.loadUrl(url);
-                return true;
-            }
-        });
-        webView.loadUrl("http://www.baidu.com");
 
     }
 }
