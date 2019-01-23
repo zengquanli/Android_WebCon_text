b25fc6a024a52cb3bb644852a3b75cbc5fb188de
zengquan_li <1104430777@qq.com>
2019-01-23 20:40:44 +0800

    修改 XML中的文件

--- app/src/main/java/com/example/a1104/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a1104/myapplication/MainActivity.java
@@ -17,87 +17,13 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
     TextView responseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button sendRequest = (Button) findViewById(R.id.send_request);
-        responseText = (TextView) findViewById(R.id.response_text);
-        sendRequest.setOnClickListener(this);
 
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.send_request) {
-            sendRequestWithHTTPURLConnection();
-        }
-
-    }
-
-    private void sendRequestWithHTTPURLConnection() {
-        //开启线程
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
-                try {
-                    URL url = new URL("http://www.baidu.com");
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    InputStream in = connection.getInputStream();
-                    // 获取输入流
-                    reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
-                    }
-                    shouldShowRequestPermissionRationale(response.toString());
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                    if (connection != null) {
-                        try {
-                            reader.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
-                }
-            }
-        }).start();
-    }
-    private void showResponse(final String response){
-        runOnUiThread(new Runnable(){
-            @Override
-            public void run(){
-                responseText.setText(response);
-            }
-        });
     }
 }
